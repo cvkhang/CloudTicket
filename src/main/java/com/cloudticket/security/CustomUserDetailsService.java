f67041e65e3f5e81ea71ce878f17c1decbd9fa6c
cvkhang <chuvinhkhang.chukid@gmail.com>
2026-08-12 19:53:00 +0700

    feat(auth): Implement JwtService and SecurityConfig

---
+++ src/main/java/com/cloudticket/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.cloudticket.security;
+
+import com.cloudticket.auth.User;
+import com.cloudticket.auth.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPasswordHash(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole()))
+        );
+    }
+}
