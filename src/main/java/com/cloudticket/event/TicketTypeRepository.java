26a04cfcee9e385cc26e37ff03639f3a272424b7
cvkhang <chuvinhkhang.chukid@gmail.com>
2026-08-14 10:06:00 +0700

    feat(event): Add Event and TicketType repositories

---
+++ src/main/java/com/cloudticket/event/TicketTypeRepository.java
@@ -0,0 +1,11 @@
+package com.cloudticket.event;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TicketTypeRepository extends JpaRepository<TicketType, Long> {
+    List<TicketType> findByEventId(Long eventId);
+}
